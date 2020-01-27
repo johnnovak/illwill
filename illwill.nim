572d6d4afe11431931af57ae1a7d5e54fa5591be
David Krause <david@code0.xyz>
2020-01-27 17:25:24 +0100

    cleanup mouse example; mouse disable correctly; added illwillInit param
    Signed-off-by: David Krause <david@code0.xyz>

--- illwill.nim
+++ illwill.nim
@@ -32,7 +32,7 @@
 ##
 
 import macros, os, strformat, terminal, unicode
-import bitops
+import bitops, strformat # TODO mouse
 
 export terminal.terminalWidth
 export terminal.terminalHeight
@@ -40,6 +40,47 @@ export terminal.terminalSize
 export terminal.hideCursor
 export terminal.showCursor
 export terminal.Style
+
+# TODO mouse
+# https://de.wikipedia.org/wiki/ANSI-Escapesequenz
+# https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-Extended-coordinates
+# /* DECSET arguments for turning on mouse reporting modes */
+#define SET_X10_MOUSE               9
+#define SET_VT200_MOUSE             1000
+#define SET_VT200_HIGHLIGHT_MOUSE   1001
+#define SET_BTN_EVENT_MOUSE         1002
+#define SET_ANY_EVENT_MOUSE         1003
+
+#if OPT_FOCUS_EVENT
+#define SET_FOCUS_EVENT_MOUSE       1004 /* can be combined with above */
+#endif
+
+# /* Extend mouse tracking for terminals wider(taller) than 223 cols(rows) */
+#define SET_EXT_MODE_MOUSE          1005 /* compatible with above */
+#define SET_SGR_EXT_MODE_MOUSE      1006
+#define SET_URXVT_EXT_MODE_MOUSE    1015
+
+#define SET_ALTERNATE_SCROLL        1007 /* wheel mouse may send cursor-keys */
+
+#define SET_BUTTON1_MOVE_POINT      2001 /* click1 emit Esc seq to move point*/
+#define SET_BUTTON2_MOVE_POINT      2002 /* press2 emit Esc seq to move point*/
+#define SET_DBUTTON3_DELETE         2003 /* Double click-3 deletes */
+#define SET_PASTE_IN_BRACKET        2004 /* Surround paste by escapes */
+#define SET_PASTE_QUOTE             2005 /* Quote each char during paste */
+#define SET_PASTE_LITERAL_NL        2006 /* Paste "\n" as C-j */
+
+const 
+  CSI = 0x1B.chr & 0x5B.chr
+  SET_BTN_EVENT_MOUSE = "1002"
+  SET_ANY_EVENT_MOUSE = "1003"
+  SET_SGR_EXT_MODE_MOUSE = "1006"
+  ENABLE = "h"
+  DISABLE = "l"
+  mouseTrackButtonPress = fmt"{CSI}?{SET_BTN_EVENT_MOUSE}{ENABLE}{CSI}?{SET_SGR_EXT_MODE_MOUSE}{ENABLE}"
+  mouseTrackAnyEvent = fmt"{CSI}?{SET_ANY_EVENT_MOUSE}{ENABLE}{CSI}?{SET_SGR_EXT_MODE_MOUSE}{ENABLE}"
+  disableMouseTrackButtonPress = fmt"{CSI}?{SET_BTN_EVENT_MOUSE}{DISABLE}{CSI}?{SET_SGR_EXT_MODE_MOUSE}{DISABLE}"
+  disableMouseTrackAnyEvent = fmt"{CSI}?{SET_ANY_EVENT_MOUSE}{DISABLE}{CSI}?{SET_SGR_EXT_MODE_MOUSE}{DISABLE}"  
+
 
 type
   ForegroundColor* = enum   ## Foreground colors
@@ -259,6 +300,7 @@ func toKey(c: int): Key =
 var gIllwillInitialised = false
 var gFullScreen = false
 var gFullRedrawNextFrame = false
+var gMouse = false
 
 when defined(windows):
   import encodings, unicode, winlean
@@ -576,7 +618,13 @@ proc exitFullScreen() =
     eraseScreen()
     setCursorPos(0, 0)
 
-proc illwillInit*(fullScreen: bool = true) =
+proc enableMouse() = 
+  stdout.write mouseTrackButtonPress
+
+proc disableMouse() =
+  stdout.write disableMouseTrackButtonPress
+
+proc illwillInit*(fullScreen: bool = true, mouse: bool = false) =
   ## Initializes the terminal and enables non-blocking keyboard input. Needs
   ## to be called before doing anything with the library.
   ##
@@ -585,6 +633,8 @@ proc illwillInit*(fullScreen: bool = true) =
     raise newException(IllwillError, "Illwill already initialised")
   gFullScreen = fullScreen
   if gFullScreen: enterFullScreen()
+  gMouse = mouse
+  if gMouse: enableMouse()
   consoleInit()
   gIllwillInitialised = true
   resetAttributes()
@@ -600,6 +650,7 @@ proc illwillDeinit*() =
   ## If the module is not intialised, `IllwillError` is raised.
   checkInit()
   if gFullScreen: exitFullScreen()
+  if gMouse: disableMouse()
   consoleDeinit()
   gIllwillInitialised = false
   resetAttributes()
