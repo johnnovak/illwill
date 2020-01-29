307d2b0f0f14b2f2978ea54d6b005ad0ddc8a4cc
David Krause <david@code0.xyz>
2020-01-30 00:39:10 +0100

    fixed mouse example
    Signed-off-by: David Krause <david@code0.xyz>

--- examples/mouse.nim
+++ examples/mouse.nim
@@ -6,7 +6,7 @@ proc exitProc() {.noconv.} =
   showCursor()
   quit(0)
 
-illwillInit(fullscreen=true, mouse=true)
+illwillInit(fullscreen=true, mouseMode = TrackAny)
 setControlCHook(exitProc)
 hideCursor()
 
@@ -18,12 +18,21 @@ while true:
   of Key.None: discard
   of Key.Escape, Key.Q: exitProc()
   of Key.Mouse:
-    let mouse = getMouse()
-    # echo mouse
-    if mouse.action == MouseButtonAction.Pressed:
-      tb.write mouse.x, mouse.y, "#"
+    let mi = getMouse()
+    if mi.action == MouseButtonAction.Pressed:
+      case mi.button
+      of ButtonLeft:
+        if mi.ctrl:
+          tb.write mi.x, mi.y, fgRed, styleBright, "♥"
+        else:
+          tb.write mi.x, mi.y, fgRed, styleDim , "♥"
+      of ButtonMiddle:
+        tb.write mi.x, mi.y, fgBlue, "◉"
+      of ButtonRight:
+        tb.write mi.x, mi.y, fgCyan, "#"
+      else: discard
     else:
-      tb.write mouse.x, mouse.y, "^"
+      tb.write mi.x, mi.y, "^"
   else:
     echo key
     discard
